88fe3bd4872f712f5a75e110eb0e853138ede836
Maarten Pronk <git@evetion.nl>
2021-11-01 08:23:37 +0100

    Added AIDW.

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -57,3 +57,72 @@ function idw(coords, tree, r, values, coordinates, power=2)
         return μ
     end
 end
+
+"""
+Adjusted Inverse Distance Weighting (AIDW) algorithm by [^li2018].
+
+Adjusts the normal IDW by process by assigning extra weights
+based on the distribution of points, ensuring that points
+all around are used normally, but that points in a clustered
+distribution are penalized when 'shielded' by a nearby point.
+
+This is useful in interpolation SpaceLiDAR data with very irregular
+distributions.
+
+[^li2018] Li, Zhengquan, Kuo Wang, Hao Ma, and Yaoxiang Wu. 2018. ‘An Adjusted Inverse Distance Weighted Spatial Interpolation Method’. In Proceedings of the 2018 3rd International Conference on Communications, Information Management and Network Security (CIMNS 2018). Shenzhen, China: Atlantis Press. https://doi.org/10/gm9kp5.
+"""
+function aidw(coords, tree, r, values, coordinates, power=2)
+    idxs = inrange(tree, coords, r, false)
+    if length(idxs) == 0
+        return NaN32
+    else
+        distances = colwise(Euclidean(), Float32.(coords), coordinates[:,idxs])
+        s = sortperm(distances)
+        k = calc_k(distances[s], coords, coordinates[:,idxs][:, s])
+        ws = k ./ distances[s].^power # .* view(uncertainty, idxs)
+        Σw = sum(ws)
+
+        if isinf(Σw)
+            μ = values[s[1]]
+        else
+            ws ./= Σw
+            vs = view(values, idxs[s])
+            μ = sum(ws[i] * vs[i] for i in eachindex(vs))
+        end
+        return μ
+end
+end
+
+function calc_angles(coords, coordinates)
+    dv = coordinates .- coords
+    atand.(dv[2,:], dv[1,:])
+end
+
+function calc_k(distances, coords, coordinates, dist=Euclidean())
+
+    # assumes these coordinates and angles are already sorted
+    # with respect to their distance to the point to interpolate
+    angles = calc_angles(coords, coordinates[:,idxs])
+    w = ones(length(angles))
+    shield_angle = 360. / length(w)
+
+    # For each point, check if closerby points shield it
+    # and if so calculate a lower weight for it
+    for i in 2:length(w)
+        for j in 1:i - 1
+            α = abs(angles[i] % 180 - angles[j] % 180)
+            if α < shield_angle
+
+                dij = evaluate(dist, coordinates[:, i], coordinates[:, j])
+                if dij == 0 || α == 0  # i is directly behind or on j
+                    w[i] = 0
+                    break
+                end
+                θ = asind(distances[i] * sind(α) / dij) - (α / 2)
+                @info i, j, coordinates[:, i], coordinates[:, j], distances[i], distances[j], dij, α / 2, θ, distances[i] * sind(α) / dij
+                w[i] *= sind(θ)
+            end
+        end
+    end
+    return w
+end
