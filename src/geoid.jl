552f226f416e1b218bf6a4b7aefcf077b311c4d9
Maarten Pronk <git@evetion.nl>
2022-09-05 18:29:07 +0200

    Fix geoid transformation.

--- src/geoid.jl
+++ src/geoid.jl
@@ -3,7 +3,7 @@ using Downloads
 
 function to_egm2008!(table)
     Proj.enable_network!()
-    trans = Proj.Transformation("EPSG:4326", "EPSG:4326+3855")
+    trans = Proj.Transformation("EPSG:4979", "EPSG:3855")
     data = Proj.Coord.(table.y, table.x, table.z)
     return table[:, :z] .= getindex.(trans.(data), 3)
 end
